1a2e1a638a99e45bab5f93e4d9e3f319cca315d8
dibyanshu0 <dibyanshubasnet@gmail.com>
2026-02-19 18:56:54 +0545

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char op;
+
+    printf("===== BASIC CALCULATOR =====\n");
+
+    // Input
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter operator (+, -, *, /, %): ");
+    scanf(" %c", &op);   // space before %c is important
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    // Processing
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '/':
+            if(num2 != 0)
+                printf("Result = %.2f\n", num1 / num2);
+            else
+                printf("Error: Division by zero!\n");
+            break;
+
+        case '%':
+            if((int)num2 != 0)
+                printf("Result = %d\n", (int)num1 % (int)num2);
+            else
+                printf("Error: Modulus by zero!\n");
+            break;
+
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
